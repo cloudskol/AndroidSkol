f2d0206d95ca3d6567d67e035f7e502ab65b2ccf
Thamizharasu <zarub2k@gmail.com>
2016-09-18 16:54:26 +0530

    Initial version is committed

---
+++ app/src/main/java/com/cloudskol/androidskol/LocationAPIClient.java
@@ -0,0 +1,31 @@
+package com.cloudskol.androidskol;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+
+/**
+ * @author tham
+ */
+public class LocationAPIClient implements GoogleApiClient.ConnectionCallbacks, OnConnectionFailedListener {
+    private final String LOG_TAG = LocationAPIClient.class.getSimpleName();
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
